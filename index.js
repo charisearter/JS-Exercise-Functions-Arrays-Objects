91d8cf5e5c3f28aa8be15a0e00bc5fc81c45e9c2
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-04-16 16:29:42 -0500

    passed getName

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ function temperatureInF(temp, unit) {
 */
 function makePersonObject(id, name, email) {
   return {id, name, email};
-  /* code here */
+
 }
 
 /**
@@ -123,7 +123,8 @@ function makePersonObject(id, name, email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
+function getName(obj) { //objectName is the parameter
+  return `Hello, my name is ${obj.name}` //calls objectName and the value key name
   /* code here */
 }
 
